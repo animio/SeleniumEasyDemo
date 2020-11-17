a0aaea265977e15cdd6700ba42dcf7624efede1c
animio <amiovska@gmail.com>
2020-11-17 23:21:56 +0100

    commit 3

--- src/main/java/sEasyDemo/SE/Base.java
+++ src/main/java/sEasyDemo/SE/Base.java
@@ -8,14 +8,22 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pageObjects.landingPage;
 
 public class Base {
 	
 	public WebDriver driver;
+	Properties prop;
+	landingPage LP;
+	
 	
 	public WebDriver initializeDriver() throws IOException{
 		
-		Properties prop = new Properties();
+		prop = new Properties();
 		String directory = System.getProperty("user.dir");
 		FileInputStream fis = new FileInputStream(directory + "/src/main/java/sEasyDemo/SE/data.properties");
 		prop.load(fis);
@@ -29,18 +37,37 @@ public class Base {
 		if (browserName.equalsIgnoreCase("Edge")) {
 			System.setProperty("webdriver.edge.driver", "C:/Users/Anita Miovska/Selenium/Drivers/edgedriver_win64/msedgedriver.exe");
 			driver = new EdgeDriver();
-		}
-		
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		String url = prop.getProperty("url");
-		driver.get(url);
-		
-		
+		}	
 		
 		return driver;
 		
 	}
+
+	@BeforeClass
+	public void initialize() throws IOException {
+		driver = initializeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		String url = prop.getProperty("url");
+		driver.get(url);
+		LP = new landingPage(driver);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOf(LP.popupX()));
+		try {
+		LP.popupX().click();
+		}
+		catch(Exception e){
+		}
+		
+	}
 	
+	
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+		driver.quit();
+	}
+	
+
 
 }
--- src/test/java/sEasyDemo/SE/singleInputFieldPageTest.java
+++ src/test/java/sEasyDemo/SE/singleInputFieldPageTest.java
@@ -1,10 +1,9 @@
 package sEasyDemo.SE;
 
-import java.io.IOException;
+import java.util.Random;
 
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjects.landingPage;
@@ -12,27 +11,42 @@ import pageObjects.singleInputFormPage;
 
 
 
-public class singleInputFieldPageTest extends Base{
+public class singleInputFieldPageTest extends Base {
 	
-	@Test
-	public void getPage() throws IOException {
-		driver = initializeDriver();
-		landingPage LP = new landingPage(driver);
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		wait.until(ExpectedConditions.visibilityOf(LP.popupX()));
-		try {
-		LP.popupX().click();
-		}
-		catch(Exception e){
-		}
+	
+	@Test(dataProvider="data")
+	public void getPage(String text) throws Exception {
+		LP = new landingPage(driver);
 		LP.inputFormsLink().click();
 		LP.inputFormsSFD().click();
 		singleInputFormPage SIFP = new singleInputFormPage(driver);
-		SIFP.SIFenterMessageBox().sendKeys("Hello world");
+		SIFP.SIFenterMessageBox().sendKeys(text);
 		SIFP.SIFshowMessageBtn().click();
-		String textInput = SIFP.SIFyourMessageText().getText();
-		Assert.assertEquals(textInput, "Hello world");
+		Assert.assertEquals(SIFP.SIFyourMessageText().getText(), text);
+		int n = num();
+		SIFP.TIFenterValueA().sendKeys(Integer.toString(n));
+		int n1 = num();
+		SIFP.TIFenterValueB().sendKeys(Integer.toString(n1));
+		SIFP.TIFgetTotalBtn().click();
+		int sumAB = n + n1;
+		Assert.assertEquals(SIFP.TIFtotalAB().getText(), Integer.toString(sumAB));
+	
+	}
+
+	
+	private int num() {
+		Random number = new Random();
+		int n = number.nextInt(50);
+		return n;
 		
+	}
+
+
+	@DataProvider 
+	public Object[][] data() {
+		Object[][] data = new Object[1][1];
+		data[0][0] = "Hello world!";
+		return data;
 	}
 
 }
